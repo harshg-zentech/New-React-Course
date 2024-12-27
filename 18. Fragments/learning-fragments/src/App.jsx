7f0f1ebdb99d95d219b2cfc8fc4aebfa8cd66284
harsh-gandhi-007 <harsh.gandhi@zentechsolution.com>
2024-12-27 17:55:06 +0530

    Eighteenth Chapter Learning Fragments

---
+++ 18. Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default App
